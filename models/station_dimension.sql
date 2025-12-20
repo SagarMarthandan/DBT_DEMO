f0c2046d731de8b1fba44cd56f2306449b20fb7c
Sagar Marthandan <sammars2492@gmail.com>
2025-12-20 05:42:03 +0000

    changed_global _materializtaion_tables

---
+++ models/station_dimension.sql
@@ -0,0 +1,17 @@
+-- first CTE
+WITH BIKE AS (
+    select
+        start_station_id AS station_id,
+        start_station_name AS station_name,
+        start_lat AS station_lat,
+        start_lng as station_lng
+    from
+        {{ source('demo', 'bike') }}
+    where
+        RIDE_ID != 'ride_id'
+)
+
+select
+    *
+from
+    BIKE
