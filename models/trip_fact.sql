0208d62b4c0c509639838f1ebe52cb87d6cfcee3
Sagar Marthandan <sammars2492@gmail.com>
2025-12-20 05:56:51 +0000

    add trip_fact table

---
+++ models/trip_fact.sql
@@ -0,0 +1,20 @@
+-- first CTE
+WITH TRIPS AS (
+    select
+        RIDE_ID,
+        RIDEABLE_TYPE,
+        DATE(TO_TIMESTAMP(STARTED_AT)) AS TRIP_DATE,
+        start_station_id,
+        END_STATION_ID,
+        MEMBER_CSUAL AS MEMBER_CASUAL,
+        TIMESTAMPDIFF(SECOND, TO_TIMESTAMP(STARTED_AT), TO_TIMESTAMP(ENDED_AT)) AS TRIP_DURATION_SECONDS
+    from
+        {{ source('demo', 'bike') }}
+    where
+        RIDE_ID != 'ride_id'
+)
+
+select
+    *
+from
+    TRIPS
